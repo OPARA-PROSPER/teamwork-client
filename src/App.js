8dc12d76f9695f91f270e63f9fca42b2c3ff6f5b
Prosper Opara <oparaprosper79@gmail.com>
2020-02-13 16:38:55 +0100

    Add signup route

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter, Route} from 'react-router-dom';
 import Login from './containers/login/login';
 import Dashboard from './containers/dashboard/dashboard';
+import Signup from './containers/signup/signup';
 import './App.css';
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
     <BrowserRouter>
       <Route exact path="/" component={Login}/>
       <Route exact path="/dashboard/:id" component={Dashboard}/>
+      <Route exact path="/signup" component={Signup}/>
     </BrowserRouter>
   );
 }
