5878ad15649b35d2f8b150d31b683d311016f106
Prosper Opara <oparaprosper79@gmail.com>
2020-02-17 03:36:18 +0100

    Add new route

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter, Route} from 'react-router-dom';
 import Login from './containers/login/login';
 import Dashboard from './containers/dashboard/dashboard';
+import UserArticleById from './components/articles/view/articlesById/userArticlesById';
 import Signup from './containers/signup/signup';
 import './App.css';
 
@@ -10,6 +11,7 @@ function App() {
     <BrowserRouter>
       <Route exact path="/" component={Login}/>
       <Route exact path="/dashboard/:id" component={Dashboard}/>
+      <Route exact path="/dashboard/:id/articles/:articleId" component={UserArticleById}/>
       <Route exact path="/signup" component={Signup}/>
     </BrowserRouter>
   );
